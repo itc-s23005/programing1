387715a8cc2824c73232966bfd08b91ea5708a20
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-07-13 00:27:01 +0000

    first

---
+++ CHAPTER2/Q2_9_2.py
@@ -0,0 +1,17 @@
+x = True
+y = False
+z = None
+
+xandzisNone
+notxornoty
+xandnotyandzisNone
+
+True
+True
+True
+x = True
+y = False
+z = None
+print(x and z is None)
+print(not x or not y)
+print(x and not y and z is None)
