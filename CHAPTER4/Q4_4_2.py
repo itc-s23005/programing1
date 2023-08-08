0d771eea976f97ad2270b415145165d01b30e5ad
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-08 06:07:31 +0000

    first

---
+++ CHAPTER4/Q4_4_2.py
@@ -0,0 +1,15 @@
+vote_num = 0
+
+def vote(vote_n):
+    print("投票します")
+    vote_n += 1
+    return vote_n
+
+def result_box(vote_n):
+    print("箱を空にします")
+    vote_n = 0
+    return vote_n
+
+def check_box(vote_n):
+    print("票の数は　{}　です" .format(vote_n))
+    return
