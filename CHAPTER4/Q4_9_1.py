5c8ea88159371738085b9b2222529ebb4fbdce3f
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-08 06:46:23 +0000

    first

---
+++ CHAPTER4/Q4_9_1.py
@@ -0,0 +1,12 @@
+# 悪い例1
+spam( ham[ 1 ], { eggs: 2 } )
+
+# 悪い例2
+foo= (0, )
+
+#悪い例3
+if x == 4 : print (x , y) : x , y = y , x
+
+spam(ham[1], (eggs: 2})
+foo = (0,)
+if x == 4: print(x, y); x, y = y, x
