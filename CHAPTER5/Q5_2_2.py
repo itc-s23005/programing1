ecd9e29471de4c48ab062b78a45b515f2f60f8f0
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-10 06:57:49 +0000

    first

---
+++ CHAPTER5/Q5_2_2.py
@@ -0,0 +1,12 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+[x if x%2==0 else None for x in list]
+
+1[4, 6, 8]
+
+2[1, None, 3, None, 5, None, 7, None, 9]
+
+3[[None, 2, None, 4, None, 6, None, 8, None]]
+
+4[None, 2, None, 4, None, 6, None, 8, None]
+
+4
