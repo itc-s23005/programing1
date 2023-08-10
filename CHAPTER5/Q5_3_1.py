444714e11e7438f2158f28872b9020c831d0555b
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-10 06:59:34 +0000

    first

---
+++ CHAPTER5/Q5_3_1.py
@@ -0,0 +1,13 @@
+members = ['Yamada', 'Satou', 'Suzuki', 'Tanaka', 'Itou']
+del members[2]
+members
+
+1 ['Yamada', 'Suzuki', 'Tanaka', 'Itou']
+
+2 ['Yamada', 'Satou', 'Tanaka', 'Itou']
+
+3 ['Yamada', 'Satou', 'Suzuki', 'Itou']
+
+4 ['Yamada', 'Satou', 'Suzuki', 'Tanaka', 'Itou']
+
+2
