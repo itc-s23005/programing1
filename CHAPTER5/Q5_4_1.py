b8b71577616b078229a7fcb5ea622afed1d05281
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-10 07:01:17 +0000

    first

---
+++ CHAPTER5/Q5_4_1.py
@@ -0,0 +1,15 @@
+以下のプログラムを実行した際の出力結果として、正しいものはどれでしょうか？
+
+a, b, c, d, e = ('Sapporo'. 'Sendai'. 'Tokyo'. 'Nagoya'. 'Osaka')
+g = d + e
+g
+
+1 'Nagoya'
+
+2 'Nagoya','Osaka'
+
+3 'NagoyaOsaka'
+
+4 "
+
+3
