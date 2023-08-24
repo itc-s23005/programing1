47aa7183b454481118419b9b4a6fd327025a31c7
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-24 06:54:14 +0000

    first

---
+++ CHAPTER5/Q5_8_2.py
@@ -0,0 +1,18 @@
+下記の国番号、会員場号、会員の性別、氏名、年齢、都道府県の表をもとに、データを格納する辞書を生成するプログラムを作成してください
+まず、表を入れ子のリストデータとして生成し、国番号と会員場号を辞書のキー、それ以外のレコードをリスト変数として辞書の値とし、辞書変数に格納するものとします
+
+'01' '0001' Male Yamada Tarou 25 Tokyo
+
+'01' '0002' Male Satou Takashi 27 Kanagawa
+
+'01' '0003' Female Tanaka Yuko 25 Saitama
+
+'02' '0001' Male Smith Mike 22 NewJersey
+
+'02' '0002' Male Turner Tom 27 Kansas
+
+'02' '0003' Male Jackson David 25 Florida
+
+# 表データの生成
+data = [
+   ['01', '0001     
