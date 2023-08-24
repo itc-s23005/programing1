b48c0d2889c6da751be4a2c2a9f3826980a20ff2
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-24 06:51:09 +0000

    first

---
+++ CHAPTER5/Q5_7_1.py
@@ -0,0 +1,18 @@
+Python 3.5で実行した場合、次のプログラムを実行したときの出力結果として、正しいのはどれでしょうか？
+
+
+yamanote_line = (0: 'Tokyo' , 1: 'Yuurakutyou' , 2: 'Shinbashi')
+for v in yamanote_line.values():
+    print(v)
+
+
+1 順不同　Tokyo Yuurakutyou Shinbashi
+
+2 順不同　0 1 2
+
+3 順不同　0 Tokyo 1 Yurakutyou 2 Shinbashi
+
+4 Tokyo Yuurakutyou Shinbashi
+
+1
+
