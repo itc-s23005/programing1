00bfb1872f85d76ca4011c654a74493fcd6b6838
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-30 12:26:05 +0000

    first

---
+++ CHAPTER7/Q7_4_2.py
@@ -0,0 +1,31 @@
+ def test_function():
+     try:
+         print('try')
+         return
+     except:
+         print('except')
+     else:
+         print('else')
+     finally:
+         print('finally')
+
+ test_function()        
+
+
+ 1 try
+   finally
+
+ 2 try
+   except
+   else
+   finally
+
+ 3 try
+   else
+   finally
+
+ 4 finally
+
+
+ 1
+
