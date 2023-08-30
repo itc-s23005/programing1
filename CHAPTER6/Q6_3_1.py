cb81e62359020f0695c62b32be11626be1c1fa7a
Kyan Chihiro <s23005@std.it-college.ac.jp>
2023-08-30 12:14:08 +0000

    first

---
+++ CHAPTER6/Q6_3_1.py
@@ -0,0 +1,8 @@
+class Katsuo(Nigiri):
+    top = "かつお"
+    topping = "生姜とねぎ"
+    price = 100
+
+    def show_attributes(self):
+        super().show_attributes()
+        print("topping: {}".format(self.topping))
